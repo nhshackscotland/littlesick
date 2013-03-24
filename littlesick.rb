7f16af8cf0113bcf592a2874c57d8a0808af184a
Alan Gardner <urfolomeus@gmail.com>
2013-03-24 14:49:17 +0000

    Demo readyga

--- littlesick.rb
+++ littlesick.rb
@@ -15,6 +15,7 @@ class LittleSick < Sinatra::Base
     @location_name = service[:location_name]
     @phone = service[:phone]
     @address = service[:address]
+    @category = service[:category]
 
     erb :details, layout: :application
   end
@@ -22,15 +23,40 @@ class LittleSick < Sinatra::Base
   private
 
   def results
+    # REPLACE WITH REAL DATA!!
     [
       {
         _id: "514eece3005385f0cdd250f2",
         location_name: "City Centre MIU",
         distance: 3.2,
         phone: "0845 678 9107",
-        address: "12 Old Dalkeith Road\nEdinburgh\nEH15 2HR",
+        address: "3 Cowgate\nEdinburgh\nEH02 2RT",
         category: "MIU"
-      }
-    ]
+      },
+      {
+        _id: "314eece3005385f0cdd250f2",
+        location_name: "Royal A&E",
+        distance: 2.1,
+        phone: "0131 678 9107",
+        address: "12 Old Dalkeith Road\nEdinburgh\nEH15 2HR",
+        category: "AAE"
+      },
+      {
+        _id: "714eece3005385f0cdd250f2",
+        location_name: "Liberton MIU",
+        distance: 2.8,
+        phone: "0131 578 9330",
+        address: "58 Liberton Road\nEdinburgh\nEH15 2HR",
+        category: "MIU"
+      },
+      {
+        _id: "814eece3005385f0cdd250f2",
+        location_name: "Western General Hospital",
+        distance: 3.8,
+        phone: "0131 478 0077",
+        address: "Queensferry Road\nEdinburgh\nEH15 2HR",
+        category: "HOS"
+      },
+    ].sort{|a,b| a[:distance] <=> b[:distance] }
   end
 end
