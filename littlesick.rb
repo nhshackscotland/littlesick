722323a004c0ceb41da8695ba462ace5b5fd1b86
Alan Gardner <urfolomeus@gmail.com>
2013-03-24 14:33:09 +0000

    Pretty much working with dummy data

--- littlesick.rb
+++ littlesick.rb
@@ -6,10 +6,31 @@ class LittleSick < Sinatra::Base
   end
 
   get '/results' do
+    @results = results
     erb :results, layout: :application
   end
 
-  get '/details' do
+  get '/details/:id' do
+    service = results.find{|result| result[:_id] == params[:id]}
+    @location_name = service[:location_name]
+    @phone = service[:phone]
+    @address = service[:address]
+
     erb :details, layout: :application
+  end
+
+  private
+
+  def results
+    [
+      {
+        _id: "514eece3005385f0cdd250f2",
+        location_name: "City Centre MIU",
+        distance: 3.2,
+        phone: "0845 678 9107",
+        address: "12 Old Dalkeith Road\nEdinburgh\nEH15 2HR",
+        category: "MIU"
+      }
+    ]
   end
 end
