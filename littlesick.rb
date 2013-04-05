3ecd2ae6709c3fa992568b682a3b9f7fc0129d85
Alan Gardner <urfolomeus@gmail.com>
2013-04-05 15:30:09 +0100

    Adding map functionality.

--- littlesick.rb
+++ littlesick.rb
@@ -1,69 +1,100 @@
 require 'sinatra'
 require 'sinatra/content_for'
+require 'json'
 
 class LittleSick < Sinatra::Base
   helpers Sinatra::ContentFor
 
+  enable :sessions
+
   get '/' do
+    @categories = [
+      { key: "HOS", name: "Hospital", class: category_class('HOS') },
+      { key: "AAE", name: "A&E", class: category_class('AAE') },
+      { key: "MIU", name: "MIU", class: category_class('MIU') },
+      { key: "GPP", name: "GP", class: category_class('GPP') },
+      { key: "PHA", name: "Pharmacy", class: category_class('PHA') }
+    ]
     erb :filter, layout: :application
   end
 
   get '/map' do
+    @services = results.to_json
     erb :map, layout: :application
   end
 
   get '/results' do
+    session[:ll] = params[:ll] if params.has_key?('ll')
+    session[:categories] = params[:categories] if params.has_key?('categories')
+
     @results = results
+    position = session[:ll].split(',').map(&:to_f)
+    @results.each do |result|
+      result["distance"] = distance(position, result["location"])
+    end
     erb :results, layout: :application
   end
 
   get '/details/:id' do
-    service = results.find{|result| result[:_id] == params[:id]}
-    @location_name = service[:location_name]
-    @phone = service[:phone]
-    @address = service[:address]
-    @category = service[:category]
+    service = results.find{|service| service["_id"] == params[:id]}
+    @location_name = service["location_name"]
+    @phone = service["phone"]
+    @address = service["address"]
+    @category = service["category"]
 
     erb :details, layout: :application
   end
 
+  get "/data.json" do
+    content_type :json
+    results.to_json
+  end
+
   private
 
+  def category_class(key)
+    if session[:categories]
+      if session[:categories].split(',').include?(key)
+        'active'
+      else
+        ''
+      end
+    else
+      'active'
+    end
+  end
+
   def results
-    # REPLACE WITH REAL DATA!!
-    [
-      {
-        _id: "514eece3005385f0cdd250f2",
-        location_name: "City Centre MIU",
-        geo_near_distance: 3.2,
-        phone: "0845 678 9107",
-        address: "3 Cowgate\nEdinburgh\nEH02 2RT",
-        category: "MIU"
-      },
-      {
-        _id: "314eece3005385f0cdd250f2",
-        location_name: "Royal A&E",
-        geo_near_distance: 2.1,
-        phone: "0131 678 9107",
-        address: "12 Old Dalkeith Road\nEdinburgh\nEH15 2HR",
-        category: "AAE"
-      },
-      {
-        _id: "714eece3005385f0cdd250f2",
-        location_name: "Liberton MIU",
-        geo_near_distance: 2.8,
-        phone: "0131 578 9330",
-        address: "58 Liberton Road\nEdinburgh\nEH15 2HR",
-        category: "MIU"
-      },
-      {
-        _id: "814eece3005385f0cdd250f2",
-        location_name: "Western General Hospital",
-        geo_near_distance: 3.8,
-        phone: "0131 478 0077",
-        address: "Queensferry Road\nEdinburgh\nEH15 2HR",
-        category: "HOS"
-      },
-    ].sort{|a,b| a[:geo_near_distance] <=> b[:geo_near_distance] }
+    JSON.parse(File.read('data/services.json'))
+      .sort{|a,b| a[:geo_near_distance] <=> b[:geo_near_distance] }
+      .take(5)
+  end
+
+  RHO = 3960.0 # earth diameter in miles
+
+  def distance(coords_1, coords_2)
+    lat_1, lon_1 = coords_1
+    lon_2, lat_2 = coords_2 # stupid mongoDb returns backwards coords
+
+    # convert latitude and longitude to spherical coordinates in radians
+
+    # phi = 90 - latitude
+    phi_1 = (90.0 - lat_1) * Math::PI / 180.0
+    phi_2 = (90.0 - lat_2) * Math::PI / 180.0
+
+    # theta = longitude
+    theta_1 = lon_1 * Math::PI / 180.0
+    theta_2 = lon_2 * Math::PI / 180.0
+
+    # compute spherical distance from spherical coordinates
+
+    # arc length = \arccos(\sin\phi\sin\phi'\cos(\theta-\theta') + \cos\phi\cos\phi')
+    # distance = rho times arc length
+    distance = RHO * Math.acos(
+      Math.sin(phi_1) * Math.sin(phi_2) * Math.cos(theta_1 - theta_2) + Math.cos(phi_1) * Math.cos(phi_2)
+    )
+
+    # get rid of superfluous precision
+    distance.round(2)
   end
 end
---
+++ public/javascripts/map.js
@@ -0,0 +1,45 @@
+var loadMap = function () {
+  var currentPosition = new google.maps.LatLng(
+    $('#map').data('latitude'),
+    $('#map').data('longitude')
+  );
+
+  var map = new google.maps.Map($("#map")[0], {
+    zoom      : 8,
+    center    : currentPosition,
+    mapTypeId : google.maps.MapTypeId.ROADMAP
+  });
+
+  var marker = new google.maps.Marker({
+    position : currentPosition,
+    map      : map,
+    title    : 'Current location'
+  });
+
+  var plotService = function (i, service) {
+    var name = service.location_name;
+    var id   = service._id;
+    var lat  = service.latitude;
+    var long = service.longitude;
+
+    var infowindow = new google.maps.InfoWindow({
+      content: '<div id="content"><a href="/details/' + id + '">' + name + '</a></div>'
+    });
+
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(lat, long),
+      map: map,
+      title: name
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map, marker);
+    });
+  };
+
+  $.getJSON('/data.json', function(data) { $.each(data, plotService) });
+};
+
+window.addEventListener('push', function() {
+  if($('#map').length) { loadMap() }
+});
